46885e64cc1ef2a0aa0f76dc820804d4ec8d1537
fajar diki <fajarsiddiqi@gmail.com>
2019-09-30 16:27:17 -0700

    nyoba

--- application/views/efos/V_timeinmarket.php
+++ application/views/efos/V_timeinmarket.php
@@ -1,1 +1,33 @@
-<h1>Time In Market</h1>
+
+<script>
+var options = {
+  chart: {
+    renderTo: 'container',
+  },
+
+  xAxis: {
+    type: 'datetime',
+    labels: {
+      format: '{value:%Y-%m-%d}'
+    }
+  },
+  
+  yAxis: {
+  	type: 'datetime',
+    labels: {
+      format: '{value:%H:%M:%S}',
+    }
+  },
+
+  series: [{
+    data: [
+      [Date.parse('2016-10-20 03:14'), Date.parse('2016-10-20 03:14')],
+      [Date.parse('2016-10-21 04:14'), Date.parse('2016-10-20 04:32')],
+      [Date.parse('2016-10-22 12:14'), Date.parse('2016-10-20 05:34')]
+    ],
+    type: 'line'
+  }]
+}
+
+var chart = Highcharts.chart(options);
+</script>
